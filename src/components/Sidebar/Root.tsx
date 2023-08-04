7e44116e3742f124e05736e8956d8aef0b3bf9de
Matheus Santana <matheussantanaoli@gmail.com>
2023-08-03 23:11:22 -0300

    style: adjusted the z-index of the root

--- src/components/Sidebar/Root.tsx
+++ src/components/Sidebar/Root.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Root = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.aside
-      className="flex flex-col justify-between  w-[250px] md:w-[500px] fixed right-0 top-0 h-screen bg-white"
+      className="flex flex-col justify-between  w-[250px] md:w-[500px] fixed right-0 top-0 h-screen bg-white z-50"
       initial={{ opacity: 0, right: "-100%" }}
       animate={{ opacity: 1, right: 0 }}
       transition={{ duration: 0.4 }}
