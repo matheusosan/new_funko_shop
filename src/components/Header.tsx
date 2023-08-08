ce978403e6c95d009f6cf6dbf9f2a1c68f07b635
Matheus Santana <matheussantanaoli@gmail.com>
2023-08-08 00:45:46 -0300

    chore: added cart length to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,12 @@ import React from "react";
 import Link from "next/link";
 import * as I from "react-icons/ai";
 import { AnimatePresence } from "framer-motion";
-import { useSidebar } from "@/state";
+import { useShoppingCart, useSidebar } from "@/state";
 import { SidebarProvider } from "./Sidebar";
 
 const Header = () => {
   const { isOpen, toggleSidebar } = useSidebar();
+  const { cart } = useShoppingCart();
 
   return (
     <header className="flex items-center justify-between w-full p-7 h-[8vh] bg-blue-900">
@@ -18,8 +19,12 @@ const Header = () => {
       <Link className="text-white" href="/explore">
         Shop
       </Link>
-      <button onClick={() => toggleSidebar()}>
+      <button
+        className="flex items-center gap-2"
+        onClick={() => toggleSidebar()}
+      >
         <I.AiOutlineShoppingCart size={26} color="white" />
+        <span className="text-white">{cart.length}</span>
       </button>
 
       <AnimatePresence>
